c56c1b6469c2618c612297273630f0eb7494fe8a
andreewaD <51123966+andreewD@users.noreply.github.com>
2021-11-02 21:14:28 -0500

    Modal homeworks

---
+++ src/Components/Modal/AnnouncementsModal.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import {
+    MDBBtn,
+    MDBModal,
+    MDBModalDialog,
+    MDBModalContent,
+    MDBModalHeader,
+    MDBModalTitle,
+    MDBModalBody,
+    MDBModalFooter,
+} from 'mdb-react-ui-kit';
+function AnnouncementsModal() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AnnouncementsModal
