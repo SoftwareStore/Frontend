5ebf76d906ebd8eacf3d1f7cf2cdc3176179f6b5
banicaman <84487659+banicaman@users.noreply.github.com>
2021-12-13 21:44:13 -0500

    *
    .

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from '../../Components/Header/HeaderSesion';
 import HomeTemplate from '../../Components/Home/Home2'
-import Footer from '../../Components/Footer/Footer1'
+import Footer from '../../Components/Footer/Footer2'
 
 function Home() {
     return (
@@ -9,7 +9,7 @@ function Home() {
         <div>
             <Header />
             <HomeTemplate />
-            <Footer/>
+            <Footer />            
         </div>
     )
 }
